9fbb6b6da82dd15173081b0df3889c0ef72cc4b8
mircheus <mircheuss@yandex.ru>
2022-07-21 00:56:42 +0300

    добавлено приватное поле типа Coroutine - _alarmFader.
    Методы TurnOnSignal и TurnOffSignal переименованы в TurnOnAlarm, TurnOffAlarm. Внутри методы добавлены проверки поля _alarmFader на null  значение. Если _alarmFader != null то текущая корутина останавливается и после запускается следующая.

--- Assets/Scripts/VolumeFader.cs
+++ Assets/Scripts/VolumeFader.cs
@@ -8,6 +8,7 @@ public class VolumeFader : MonoBehaviour
 {
     [SerializeField] private float _seconds = 1f;
     [SerializeField] private AudioSource _audioSource;
+    private Coroutine _alarmFader;
     
     void Start()
     {
@@ -15,24 +16,37 @@ public class VolumeFader : MonoBehaviour
         _audioSource.Play();
     }
 
-    public void TurnOnSignal()
-    {
-        StartCoroutine(FadeVolumeTo(1));
+    
+    public void TurnOnAlarm()
+    { 
+        if (_alarmFader != null)
+        {
+            Debug.Log("coroutine stopped in TurnOnSignal");
+            StopCoroutine(_alarmFader);
+        }
+        
+        _alarmFader = StartCoroutine(FadeVolumeTo(1));
     }
 
-    public void TurnOffSignal()
+    public void TurnOffAlarm()
     {
-        StartCoroutine(FadeVolumeTo(0));
+        if (_alarmFader != null)
+        {
+            Debug.Log("coroutine stopped in TurnOffSignal");
+            StopCoroutine(_alarmFader);
+        }
+        
+        _alarmFader  = StartCoroutine(FadeVolumeTo(0));
     }
 
-    private IEnumerator FadeVolumeTo(float target)
+    private IEnumerator FadeVolumeTo(float targetVolume)
     {
         var waitFor = new WaitForSeconds(_seconds);
         float volumeDelta = 0.1f;
         
-        while (_audioSource.volume != target)
+        while (_audioSource.volume != targetVolume)
         { 
-            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, target, volumeDelta);
+            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, targetVolume, volumeDelta);
             yield return waitFor;
         }
     }
